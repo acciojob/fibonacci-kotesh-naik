c75dbf040808826aea950ca1f86ccbf3c9c70c44
kotesh-naik <160763127+kotesh-naik@users.noreply.github.com>
2024-06-16 09:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function fibonacci(num) {
 	if(num === 0 || num === 1)
 		return num;
 	//recursive case
-	return fibonacci(num - 1) + fibonacci(num - 2);
+	return fibonacci(num -1) + fibonacci(num -2);
 }
 
 module.exports = fibonacci;
